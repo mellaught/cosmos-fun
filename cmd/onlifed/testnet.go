e7b6b9e3eea8a88374ed9c452399f8cbb72d143b
mellaught <torres-dan@yandex.ru>
2020-08-26 19:23:03 +0300

    update testnet

---
+++ cmd/onlifed/testnet.go
@@ -0,0 +1,383 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"path/filepath"
+	"time"
+
+	flags "github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/keys"
+	"github.com/cosmos/cosmos-sdk/codec"
+	keyring "github.com/cosmos/cosmos-sdk/crypto/keys"
+	"github.com/cosmos/cosmos-sdk/server"
+	srvconfig "github.com/cosmos/cosmos-sdk/server/config"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	genaccounts "github.com/cosmos/cosmos-sdk/x/genaccounts/legacy/v0_36"
+	"github.com/cosmos/cosmos-sdk/x/genutil"
+	typessdk "github.com/cosmos/cosmos-sdk/x/genutil/types"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	tmconfig "github.com/tendermint/tendermint/config"
+	"github.com/tendermint/tendermint/crypto"
+	cmn "github.com/tendermint/tendermint/libs/os"
+	"github.com/tendermint/tendermint/types"
+)
+
+var (
+	flagNodeDirPrefix     = "node-dir-prefix"
+	flagNumValidators     = "v"
+	flagOutputDir         = "output-dir"
+	flagNodeDaemonHome    = "node-daemon-home"
+	flagNodeCLIHome       = "node-cli-home"
+	flagStartingIPAddress = "starting-ip-address"
+	flagBaseport          = "base-port"
+	flagLocal             = "local"
+	testnetAccountList    []string
+)
+
+// get cmd to initialize all files for tendermint testnet and application
+func testnetCmd(ctx *server.Context, cdc *codec.Codec,
+	mbm module.BasicManager, genAccIterator genutil.GenesisState,
+) *cobra.Command {
+
+	cmd := &cobra.Command{
+		Use:   "testnet",
+		Short: "Initialize files for a OKChaind testnet",
+		Long: `testnet will create "v" number of directories and populate each with
+necessary files (private validator, genesis, config, etc.).
+
+Note, strict routability for addresses is turned off in the config file.
+
+Example:
+	okchaind testnet --v 4 --output-dir ./output --starting-ip-address 192.168.10.2 -l
+	`,
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			config := ctx.Config
+
+			outputDir := viper.GetString(flagOutputDir)
+			chainID := viper.GetString(flags.FlagChainID)
+			minGasPrices := viper.GetString(server.FlagMinGasPrices)
+			nodeDirPrefix := viper.GetString(flagNodeDirPrefix)
+			nodeDaemonHome := viper.GetString(flagNodeDaemonHome)
+			nodeCLIHome := viper.GetString(flagNodeCLIHome)
+			startingIPAddress := viper.GetString(flagStartingIPAddress)
+			numValidators := viper.GetInt(flagNumValidators)
+			isLocal := viper.GetBool(flagLocal)
+
+			return InitTestnet(cmd, config, cdc, mbm, genAccIterator, outputDir, chainID,
+				minGasPrices, nodeDirPrefix, nodeDaemonHome, nodeCLIHome, startingIPAddress, numValidators, isLocal)
+		},
+	}
+
+	cmd.Flags().Int(flagNumValidators, 4,
+		"Number of validators to initialize the testnet with")
+	cmd.Flags().StringP(flagOutputDir, "o", "./mytestnet",
+		"Directory to store initialization data for the testnet")
+	cmd.Flags().String(flagNodeDirPrefix, "node",
+		"Prefix the directory name for each node with (node results in node0, node1, ...)")
+	cmd.Flags().String(flagNodeDaemonHome, "okchaind",
+		"Home directory of the node's daemon configuration")
+	cmd.Flags().String(flagNodeCLIHome, "okchaincli",
+		"Home directory of the node's cli configuration")
+	cmd.Flags().String(flagStartingIPAddress, "192.168.0.1",
+		"Starting IP address (192.168.0.1 results in persistent peers list ID0@192.168.0.1:46656, ID1@192.168.0.2:46656, ...)")
+	cmd.Flags().String(
+		flags.FlagChainID, "", "genesis file chain-id, if left blank will be randomly created")
+	cmd.Flags().String(
+		server.FlagMinGasPrices, "0.00000001"+sdk.DefaultBondDenom,
+		"Minimum gas prices to accept for transactions; All fees in a tx must meet this minimum (e.g. 0.01photino,0.001stake)")
+	cmd.Flags().Int(flagBaseport, 26656, "testnet base port")
+	cmd.Flags().BoolP(flagLocal, "l", false, "run all nodes on local host")
+	return cmd
+}
+
+const nodeDirPerm = 0755
+
+// Initialize the testnet
+func InitTestnet(cmd *cobra.Command, config *tmconfig.Config, cdc *codec.Codec,
+	mbm module.BasicManager, genAccIterator genutil.GenesisState,
+	outputDir, chainID, minGasPrices, nodeDirPrefix, nodeDaemonHome,
+	nodeCLIHome, startingIPAddress string, numValidators int, isLocal bool) error {
+
+	monikers := make([]string, numValidators)
+	nodeIDs := make([]string, numValidators)
+	valPubKeys := make([]crypto.PubKey, numValidators)
+
+	okchainConfig := srvconfig.DefaultConfig()
+	okchainConfig.MinGasPrices = minGasPrices
+
+	var (
+		accs     genaccounts.GenesisState
+		genFiles []string
+	)
+
+	// generate private keys, node IDs, and initial transactions
+	for i := 0; i < numValidators; i++ {
+		nodeDirName := fmt.Sprintf("%s%d", nodeDirPrefix, i)
+		nodeDir := filepath.Join(outputDir, nodeDirName, nodeDaemonHome)
+		clientDir := filepath.Join(outputDir, nodeDirName, nodeCLIHome)
+		gentxsDir := filepath.Join(outputDir, "gentxs")
+
+		config.SetRoot(nodeDir)
+		config.RPC.ListenAddress = "tcp://0.0.0.0:26657"
+
+		if err := os.MkdirAll(filepath.Join(nodeDir, "config"), nodeDirPerm); err != nil {
+			_ = os.RemoveAll(outputDir)
+			return err
+		}
+
+		if err := os.MkdirAll(clientDir, nodeDirPerm); err != nil {
+			_ = os.RemoveAll(outputDir)
+			return err
+		}
+
+		monikers = append(monikers, nodeDirName)
+		config.Moniker = nodeDirName
+
+		ip, err := getIP(0, startingIPAddress)
+		if !isLocal {
+			ip, err = getIP(i, startingIPAddress)
+		}
+		if err != nil {
+			_ = os.RemoveAll(outputDir)
+			return err
+		}
+
+		nodeIDs[i], valPubKeys[i], err = genutil.InitializeNodeValidatorFiles(config)
+		if err != nil {
+			_ = os.RemoveAll(outputDir)
+			return err
+		}
+
+		baseport := viper.GetInt(flagBaseport)
+		port := baseport + i*100
+		if !isLocal {
+			port = baseport
+		}
+		memo := fmt.Sprintf("%s@%s:%d", nodeIDs[i], ip, port) //okdex
+		genFiles = append(genFiles, config.GenesisFile())
+
+		keyPass := "12345678"
+		kb := keyring.New("", clientDir, nil)
+		addr, secret, err := server.GenerateSaveCoinKey(kb, nodeDirName, keyPass, true)
+		if err != nil {
+			_ = os.RemoveAll(outputDir)
+			return err
+		}
+
+		fmt.Printf("clientDir: %s\n", clientDir)
+		fmt.Printf("nodeDirName: %s\n", nodeDirName)
+		fmt.Printf("addr: %s\n", addr)
+		fmt.Printf("secret: %s\n", secret)
+		fmt.Printf("-------------------\n")
+
+		info := map[string]string{"secret": secret}
+
+		cliPrint, err := json.Marshal(info)
+		if err != nil {
+			return err
+		}
+
+		// save private key seed words
+		if err := writeFile(fmt.Sprintf("%v.json", "key_seed"), clientDir, cliPrint); err != nil {
+			return err
+		}
+
+		coins, err := sdk.ParseCoins("9000000" + sdk.DefaultBondDenom)
+		if err != nil {
+			return err
+		}
+
+		accs = append(accs, genaccounts.GenesisAccount{
+			Address: addr,
+			Coins:   coins,
+		})
+
+		minSelfDelegation := sdk.MustNewDecFromStr("0.001")
+		msg := staking.NewMsgCreateValidator(
+			sdk.ValAddress(addr),
+			valPubKeys[i],
+			sdk.NewCoin(sdk.DefaultBondDenom, minSelfDelegation.RoundInt()),
+			staking.NewDescription(nodeDirName, "", "", "", ""),
+			staking.NewCommissionRates(minSelfDelegation, minSelfDelegation, minSelfDelegation),
+			minSelfDelegation.RoundInt(),
+		)
+		kbs, err := keys.NewKeyBaseFromDir(clientDir)
+		if err != nil {
+			return err
+		}
+		tx := auth.NewStdTx([]sdk.Msg{msg}, auth.StdFee{}, []auth.StdSignature{}, memo)
+		var input io.Reader
+		txBldr := auth.NewTxBuilderFromCLI(input).WithChainID(chainID).WithMemo(memo).WithKeybase(kbs)
+
+		signedTx, err := txBldr.SignStdTx(nodeDirName, "12345678", tx, false)
+		if err != nil {
+			_ = os.RemoveAll(outputDir)
+			return err
+		}
+
+		txBytes, err := cdc.MarshalJSON(signedTx)
+		if err != nil {
+			_ = os.RemoveAll(outputDir)
+			return err
+		}
+
+		// gather gentxs folder
+		if err := writeFile(fmt.Sprintf("%v.json", nodeDirName), gentxsDir, txBytes); err != nil {
+			_ = os.RemoveAll(outputDir)
+			return err
+		}
+
+		okchainConfigFilePath := filepath.Join(nodeDir, "config/okchaind.toml")
+		srvconfig.WriteConfigFile(okchainConfigFilePath, okchainConfig)
+	}
+
+	if err := initGenFiles(cdc, mbm, chainID, accs, genFiles, numValidators); err != nil {
+		return err
+	}
+
+	err := collectGenFiles(
+		cdc, config, chainID, monikers, nodeIDs, valPubKeys, numValidators,
+		outputDir, nodeDirPrefix, nodeDaemonHome, genAccIterator,
+	)
+	if err != nil {
+		return err
+	}
+
+	cmd.PrintErrf("Successfully initialized %d node directories\n", numValidators)
+	return nil
+}
+
+func initGenFiles(cdc *codec.Codec, mbm module.BasicManager, chainID string,
+	accs genaccounts.GenesisState, genFiles []string, numValidators int) error {
+
+	appGenState := mbm.DefaultGenesis()
+
+	// set the accounts in the genesis state
+	appGenState = typessdk.SetGenesisStateInAppState(cdc, appGenState, accs)
+
+	appGenStateJSON, err := codec.MarshalJSONIndent(cdc, appGenState)
+	if err != nil {
+		return err
+	}
+
+	genDoc := types.GenesisDoc{
+		ChainID:    chainID,
+		AppState:   appGenStateJSON,
+		Validators: nil,
+	}
+
+	// generate empty genesis files for each validator and save
+	for i := 0; i < numValidators; i++ {
+		if err := genDoc.SaveAs(genFiles[i]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func collectGenFiles(
+	cdc *codec.Codec, config *tmconfig.Config, chainID string,
+	monikers, nodeIDs []string, valPubKeys []crypto.PubKey,
+	numValidators int, outputDir, nodeDirPrefix, nodeDaemonHome string,
+	genAccIterator genutil.GenesisAccountsIterator) error {
+
+	var appState json.RawMessage
+	genesisTime := "2020-01-01T10:16:17.025816Z"
+	genTime := time.Time{}
+	err := genTime.UnmarshalText([]byte(genesisTime))
+	if err != nil {
+		return nil
+	}
+
+	for i := 0; i < numValidators; i++ {
+		nodeDirName := fmt.Sprintf("%s%d", nodeDirPrefix, i)
+		nodeDir := filepath.Join(outputDir, nodeDirName, nodeDaemonHome)
+		gentxsDir := filepath.Join(outputDir, "gentxs")
+		moniker := monikers[i]
+		config.Moniker = nodeDirName
+
+		config.SetRoot(nodeDir)
+
+		nodeID, valPubKey := nodeIDs[i], valPubKeys[i]
+		initCfg := genutil.NewInitConfig(chainID, gentxsDir, moniker, nodeID, valPubKey)
+
+		genDoc, err := types.GenesisDocFromFile(config.GenesisFile())
+		if err != nil {
+			return err
+		}
+
+		nodeAppState, err := genutil.GenAppStateFromConfig(cdc, config, initCfg, *genDoc, genAccIterator)
+		if err != nil {
+			return err
+		}
+
+		if appState == nil {
+			// set the canonical application state (they should not differ)
+			appState = nodeAppState
+		}
+
+		genFile := config.GenesisFile()
+
+		// overwrite each validator's genesis file to have a canonical genesis time
+		if err := genutil.ExportGenesisFileWithTime(genFile, chainID, nil, appState, genTime); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func getIP(i int, startingIPAddr string) (ip string, err error) {
+	if len(startingIPAddr) == 0 {
+		ip, err = server.ExternalIP()
+		if err != nil {
+			return "", err
+		}
+		return ip, nil
+	}
+	return calculateIP(startingIPAddr, i)
+}
+
+func calculateIP(ip string, i int) (string, error) {
+	ipv4 := net.ParseIP(ip).To4()
+	if ipv4 == nil {
+		return "", fmt.Errorf("%v: non ipv4 address", ip)
+	}
+
+	for j := 0; j < i; j++ {
+		ipv4[3]++
+	}
+
+	return ipv4.String(), nil
+}
+
+func writeFile(name string, dir string, contents []byte) error {
+	writePath := filepath.Join(dir)
+	file := filepath.Join(writePath, name)
+
+	if err := cmn.EnsureDir(writePath, 0700); err != nil {
+		return err
+	}
+
+	if err := cmn.WriteFile(file, contents, 0600); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getTestnetMnemonic(index int) string {
+	if len(testnetAccountList)-1 < index {
+		return ""
+	}
+
+	return testnetAccountList[index]
+}
--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.1
 	github.com/tendermint/go-amino v0.15.1
-	github.com/tendermint/tendermint v0.33.7
+	github.com/tendermint/tendermint v0.33.8
 	github.com/tendermint/tm-db v0.5.1
-	github.com/ugorji/go/codec v0.0.0-20181204163529-d75b2dcb6bc8 // indirect
+	github.com/ugorji/go v1.1.7 // indirect
 )
